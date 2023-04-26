7395f858bdfeac3d70678eff2784b5dc20752231
Clayton Karow <cj3karow@gmail.com>
2023-04-26 09:07:07 -0500

    something else

--- content/actions/open_door.cpp
+++ content/actions/open_door.cpp
@@ -8,11 +8,9 @@ Result OpenDoor::perform(Engine& engine) {
     Vec direction = actor->get_direction();
     Vec position = actor->get_position() + direction;
     Tile& tile = engine.dungeon.tiles(position);
-    if (tile.is_door()) {
-        Door& door = engine.dungeon.doors.at(position);
-        tile.walkable = true;
-        door.open();
-        actor->move_to(position);
-    }
+    Door& door = engine.dungeon.doors.at(position);
+    tile.walkable = true;
+    door.open();
+    actor->move_to(position);
     return success();
 }
