13d1aab38e84ea3ecd8922897661ef8661442c50
Clayton Karow <cj3karow@gmail.com>
2023-05-12 09:27:07 -0500

    added magic effect

--- content/weapons/spell_cast.cpp
+++ content/weapons/spell_cast.cpp
@@ -4,9 +4,11 @@
 #include "event.h"
 #include "hit.h"
 #include "lightning.h"
+#include "magic.h"
 
 Spell_cast::Spell_cast(int damage) : Weapon{"none", damage} {}
 
-void Spell_cast::use(Engine& engine, Actor&, Actor& defender) {
+void Spell_cast::use(Engine& engine, Actor& attacker, Actor& defender) {
+    engine.events.add(Magic{attacker.get_position(), 0});
     engine.events.add(Lightning{defender.get_position(), damage});
 }
--- content/weapons/wabbajack.cpp
+++ content/weapons/wabbajack.cpp
@@ -20,23 +20,21 @@ void Wabbajack::use(Engine& engine, Actor& attacker, Actor& defender) {
 
     engine.events.add(WonderStick{sprite, direction, defender, 0});
 
-    int probability{randint(0, 6)};
+    int probability{randint(0, 5)};
 
     if (probability == 0) {
         engine.events.add(Lightning{defender.get_position(), 5});
     } else if (probability == 1) {
-        engine.events.add(Fire{defender.get_position(), 2});
-    } else if (probability == 2) {
         engine.events.add(Explosion{defender.get_position(), 4});
-    } else if (probability == 3) {
+    } else if (probability == 2) {
         engine.events.add(Gas{defender.get_position(), 1});
-    } else if (probability == 4) {
+    } else if (probability == 3) {
         engine.events.add(Magic{defender.get_position(), 2});
-    } else if (probability == 5) {
+    } else if (probability == 4) {
         if (attacker.health <= attacker.max_health) {
             attacker.health += 2;
         }
-    } else if (probability == 6) {
+    } else if (probability == 5) {
         if (defender.health <= defender.max_health) {
             defender.health += 1;
         }
