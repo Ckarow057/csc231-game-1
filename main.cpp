2cee100ccce4574b25a61b799b6e0eeb7c050619
Clayton Karow <cj3karow@gmail.com>
2023-05-12 13:31:53 -0500

    mod main.cpp

--- main.cpp
+++ main.cpp
@@ -7,23 +7,24 @@ int main() {
     Settings settings = read_settings(filename);
     Engine engine{settings};
 
-    for (int i{0}; i < 4; ++i) {
+    for (int i{0}; i < 3; ++i) {  // Grunts
         engine.create_monster(Monsters::goblin());
         engine.create_monster(Monsters::skeleton());
         engine.create_monster(Monsters::zombie());
         engine.create_monster(Monsters::demon_tiny());
     }
 
-    for (int i{0}; i < 2; ++i) {
+    for (int i{0}; i < 2; ++i) {  // Special
         engine.create_monster(Monsters::shaman());
         engine.create_monster(Monsters::orc_masked());
         engine.create_monster(Monsters::dragon());
     }
 
-    for (int i{0}; i < 1; ++i) {
+    for (int i{0}; i < 1; ++i) {  // Fun Enemies
         engine.create_monster(Monsters::necromancer());
     }
-    engine.create_monster(Monsters::ogre());
+
+    engine.create_monster(Monsters::ogre());  // Boss
 
     engine.create_hero(Heros::nobody);
     engine.run();
