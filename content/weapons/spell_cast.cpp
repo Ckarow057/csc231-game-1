6bc86f31f15759f53ad2048f102b93fd12257d21
Clayton Karow <cj3karow@gmail.com>
2023-05-12 09:08:23 -0500

    changed the shaman

--- content/weapons/spell_cast.cpp
+++ content/weapons/spell_cast.cpp
@@ -5,8 +5,8 @@
 #include "hit.h"
 #include "lightning.h"
 
-Spell_cast::Spell_cast(int damage) : Weapon{"staff_green", damage} {}
+Spell_cast::Spell_cast(int damage) : Weapon{"none", damage} {}
 
-void Spell_cast::use(Engine& engine, Actor& attacker, Actor& defender) {
+void Spell_cast::use(Engine& engine, Actor&, Actor& defender) {
     engine.events.add(Lightning{defender.get_position(), damage});
 }
